2b571d0feb8df7078eb646906884cf3d6568ae37
govinmartin <91345178+govinmartin@users.noreply.github.com>
2021-11-01 02:01:18 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_controller
+{
+    public function index()
+{
+    echo "Perkenalkan<br>";
+    echo "Nama Saya Jeheskiel agovinda martin <br>";
+    echo "Kelas 19.3B.05 dengan NIM 19200640";
+}
